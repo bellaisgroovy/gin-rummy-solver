34f74dd82ab37bfcc2ff75fe239205ec01485f5d
Bella Hodgson <2770706h@student.gla.ac.uk>
2023-11-30 12:55:20 +0000

    removed @override from toString

--- src/main/java/org/example/data/PlayingCard.java
+++ src/main/java/org/example/data/PlayingCard.java
@@ -4,10 +4,12 @@ public class PlayingCard {
     private Suit suit; //number 1 to 13 1 being ace 13 being king
     private int value;
 
+    private final int min_card_value = 1;
+    private final int max_card_value = 13;
     public PlayingCard() {}
     public PlayingCard(Suit init_suit, int init_value) {
         this.suit = init_suit;
-        if (init_value > 0 && init_value < 14) {
+        if (init_value >= min_card_value && init_value <= max_card_value) {
             this.value = init_value;
         }
         else {
@@ -15,7 +17,6 @@ public class PlayingCard {
         }
     }
 
-    @Override
     public String toString() {
         return this.suit + " " + this.value;
     }
