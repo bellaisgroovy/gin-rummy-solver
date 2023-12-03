afdf45fe8de35575b2668eb5de8e6f008dce6e19
Bella Hodgson <2770706h@student.gla.ac.uk>
2023-12-03 21:37:30 +0000

    added: - setup() to take initial inputs - initHands() to initialize the players hands with minimal validation - tests for those - maybe other stuff sorry i forgot to commit earlier

--- src/main/java/org/example/logic/RummyGame.java
+++ src/main/java/org/example/logic/RummyGame.java
@@ -5,13 +5,48 @@ import org.example.data.PlayingCard;
 import java.util.ArrayList;
 
 public class RummyGame {
-    private ArrayList<PlayingCard> faceUpTable = new ArrayList<>(20);
 
-    public void playCard(PlayingCard card) {
-        faceUpTable.add(card);
+    private int noPlayers;
+    private ArrayList<PlayingCard> discardedPile = new ArrayList<>(20); // holds cards discarded
+    private PlayingCard[][] hands;
+
+    //actions
+    public void discardCard(PlayingCard card) {
+        discardedPile.add(card);
+    }
+    public void setup(int noPlayers, PlayingCard faceUpCard, PlayingCard[] playerHand) {
+        setNoPlayers(noPlayers);
+        discardCard(faceUpCard);
+        initHands(playerHand);
     }
 
-    public ArrayList<PlayingCard> getFaceUpTable(){
-        return faceUpTable;
+    //getters and setters
+    public ArrayList<PlayingCard> getDiscardedPile(){
+        return discardedPile;
     }
+    public PlayingCard[][] getHands() {
+        return hands;
+    }
+    public int getNoPlayers() {
+        return noPlayers;
+    }
+    private void setNoPlayers(int newNoPlayers) {
+        if (newNoPlayers>1) {
+            noPlayers = newNoPlayers;
+        } else {
+            throw  new IllegalArgumentException("noPlayers must be >1");
+        }
+    }
+
+    private void initHands(PlayingCard[] playerHand) {
+        //constants
+        int hand_size = 10;
+        hands = new PlayingCard[getNoPlayers()][hand_size];
+        if (playerHand.length != hand_size) {
+            throw new IllegalArgumentException("hands must contain " + hand_size + " cards");
+        } else {
+            hands[0] = playerHand;
+        }
+    }
+
 }
--- src/test/java/org/example/logic/TestRummyGame.java
+++ src/test/java/org/example/logic/TestRummyGame.java
@@ -4,14 +4,60 @@ import org.example.data.PlayingCard;
 import org.example.data.Suit;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestRummyGame {
     RummyGame game = new RummyGame();
+    PlayingCard card = new PlayingCard(Suit.HEARTS, 7);
     @Test
-    public void testPlayCard() {
-        PlayingCard card = new PlayingCard(Suit.HEARTS, 7);
-        game.playCard(card);
-        assertEquals(game.getFaceUpTable().get(0), card);
+    public void DiscardCard_ValidCard_CardDiscarded() {
+        game.discardCard(card);
+        assertEquals(game.getDiscardedPile().get(0), card);
+    }
+
+    @Test
+    public void Setup_MinValidInputs_VarsPopulated() {
+        PlayingCard[] hand = new PlayingCard[10];
+        for (int i=1; i<11; i++) {
+            hand[i-1] = new PlayingCard(Suit.HEARTS, i);
+        }
+
+        int noPlayers = 2;
+
+        ArrayList<PlayingCard> fakeDiscardedPile = new ArrayList<>();
+        fakeDiscardedPile.add(card);
+
+        game.setup(noPlayers, card, hand);
+        assertEquals(game.getHands()[0], hand);
+        assertEquals(game.getNoPlayers(), noPlayers);
+        assertEquals(game.getDiscardedPile(), fakeDiscardedPile);
+    }
+
+    @Test
+    public void Setup_OnePlayer_Error(){
+        PlayingCard[] hand = new PlayingCard[10];
+        for (int i=1; i<11; i++) {
+            hand[i-1] = new PlayingCard(Suit.HEARTS, i);
+        }
+        try {
+            game.setup(1, card, hand);
+        } catch (IllegalArgumentException exception) {
+            assertEquals("noPlayers must be >1", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void Setup_NineCardHand_Error() {
+        PlayingCard[] hand = new PlayingCard[10];
+        for (int i=1; i<10; i++) {
+            hand[i-1] = new PlayingCard(Suit.HEARTS, i);
+        }
+        try {
+            game.setup(2, card, hand);
+        } catch (IllegalArgumentException exception) {
+            assertEquals(exception.getMessage(), "hand must contain 10 cards");
+        }
     }
 }
